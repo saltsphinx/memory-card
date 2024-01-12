f4cbe9c5a81b7aa53cc1df05fd75aad40fd23e17
tremar thomas <81628964+saltsphinx@users.noreply.github.com>
2024-01-11 23:34:59 -0600

    Create Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p>
+        Project by <a href="https://github.com/saltsphinx">saltsphinx</a> at
+        github
+      </p>
+    </footer>
+  );
+}
