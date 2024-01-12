4e01e0aa656b7bf52cbfcaa093b3b8d58214db1e
tremar thomas <81628964+saltsphinx@users.noreply.github.com>
2024-01-11 23:22:36 -0600

    Add card data

---
+++ src/cardData.js
@@ -0,0 +1,14 @@
+export default [
+  'atmokinesis',
+  'bilocation',
+  'clairvoyance',
+  'cryokinesis',
+  'ergokinesis',
+  'hydrokinesis',
+  'perfectRecall',
+  'phytokinesis',
+  'precognition',
+  'pyrokinesis',
+  'telekinesis',
+  'telepathy',
+];
